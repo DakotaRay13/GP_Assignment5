5d2f2cb714fa1e3913b2323d03e4d017d4b974b2
DakotaRay13 <dakotarayonps4@gmail.com>
2021-04-03 19:21:49 -0500

    Tutorial Finished

---
+++ GP_Assignment5/Assets/Scripts/Word.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word
+{
+    public string word;
+    private int TypeIndex;
+
+    private WordDisplay display;
+
+    public Word (string _word, WordDisplay _display)
+    {
+        word = _word;
+        TypeIndex = 0;
+
+        display = _display;
+        display.SetWord(word);
+    }
+
+    public char GetNextLetter()
+    {
+        return word[TypeIndex];
+    }
+
+    public void TypeLetter()
+    {
+        TypeIndex++;
+        display.RemoveLetter();
+    }
+
+    public bool WordTyped()
+    {
+        bool wordTyped = (TypeIndex >= word.Length);
+        if(wordTyped)
+        {
+            display.RemoveWord();
+        }
+        return wordTyped;
+    }
+}
